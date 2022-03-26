cde13d21bc42432063e6b6c92f33c9a5ec78bf57
Boris <boris.blagojevicc@hotmail.com>
2022-03-26 09:07:02 +0100

    cv

--- src/js/textData.js
+++ src/js/textData.js
@@ -1,5 +1,12 @@
 export const bih = {
   navigationItem: ["O meni", "CV", "Alati", "Projecti"],
   headerTitle: "Zdravo ja sam, Boris Blagojevic",
-  toolsSection: ["Jezici i alati koje koristim"],
+  toolsSection: "Jezici i alati koje koristim",
+  aboutSection: [
+    "O meni",
+    "Rođen sam u Banoj Luci.",
+    "Zavrsio sam srednju Elektrotehničku školu Nikola Tesla u Banjoj Luci kao tehničar telekomunikacija.",
+    "Trenutno studiram softverski inžinjering na Panevropskom Univerzitetu Apeiron u Banjoj Luci.",
+  ],
+  cvSection: "Pogledajte moj CV",
 };
