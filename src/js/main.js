2d0a21c52b3db8eb9eb03af187449b79eab8dec0
Boris <boris.blagojevicc@hotmail.com>
2022-03-27 10:02:57 +0200

    change language functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,71 @@
-// import bih from "textData.js";
+import { bih, eng } from "./textData.js";
 
+// LANGUAGE SETTINGS ************************************************8
+const navBar = document.querySelector(".navigation__links-list--left");
+const btnENG = document.querySelector(".user-eng");
+const btnBIH = document.querySelector(".user-bih");
+
+// elements to change
+const navLink = document.querySelectorAll(".navigation__link");
+const heriTitle = document.querySelector(".hero__title");
+const aboutBox = document.querySelectorAll(".about__item");
+const cvSection = document.querySelector(".cv__subtitle");
+const toolsSection = document.querySelector(".tools__title");
+const contactSection = document.querySelector(".contact__title");
+const projectSection = document.querySelector(".project__title");
+
+let stateLang = JSON.parse(localStorage.getItem("language")) || eng;
+
+const changeLanguage = function (lang) {
+  // header
+  navLink.forEach((el, i) => {
+    if (i > 1) el.innerHTML = lang.navigationItem[i - 2];
+  });
+
+  heriTitle.innerHTML = `${lang.headerTitle[0]} <span class="hero__span">${lang.headerTitle[1]}</span>`;
+
+  // about
+  aboutBox.forEach((el, i) => (el.innerHTML = lang.aboutSection[i]));
+
+  // cv section
+  cvSection.innerHTML = lang.cvSection;
+
+  // tools
+  toolsSection.innerHTML = lang.toolsSection;
+
+  // contact
+  contactSection.innerHTML = lang.contactSection;
+
+  // project
+  projectSection.innerHTML = lang.projects;
+};
+
+const languageBtn = function (el) {
+  btnENG.classList.remove("active-language");
+  btnBIH.classList.remove("active-language");
+
+  if (el.target.classList[1] === "user-eng") {
+    btnENG.classList.add("active-language");
+    localStorage.setItem("language", JSON.stringify(eng));
+  } else {
+    btnBIH.classList.add("active-language");
+    localStorage.setItem("language", JSON.stringify(bih));
+  }
+
+  // refresh the page
+  window.location.reload();
+};
+
+// if (stateLang) {
+//   console.log(true);
+// }
+
+btnENG.addEventListener("click", languageBtn.bind(btnENG));
+btnBIH.addEventListener("click", languageBtn.bind(btnBIH));
+
+// *************************************************
+
+// CAROUSEL *******************************************
 const carousel = document.querySelectorAll(".project__box");
 const carouselLinks = document.querySelectorAll(".project__link");
 const btnLeft = document.querySelector(".projects__left");
@@ -39,11 +105,12 @@ document.addEventListener("keydown", function (e) {
   if (e.key === "ArrowRight") right();
 });
 
-// init
-translate(position);
-
-// date
+// date **********************************************************
 
 const crNumber = document.querySelector(".crNumber");
 const date = new Date();
 crNumber.innerHTML = date.getFullYear();
+
+// init ******************************************************
+translate(position);
+changeLanguage(stateLang);
--- src/js/textData.js
+++ src/js/textData.js
@@ -1,6 +1,6 @@
 export const bih = {
-  navigationItem: ["O meni", "CV", "Alati", "Projecti"],
-  headerTitle: "Zdravo ja sam, Boris Blagojevic",
+  navigationItem: ["O meni", "CV", "Alati", "Kontakt", "Projekti"],
+  headerTitle: ["Zdravo, ja sam", "Boris Blagojevic"],
   toolsSection: "Jezici i alati koje koristim",
   aboutSection: [
     "O meni",
@@ -11,4 +11,19 @@ export const bih = {
   cvSection: "Pogledajte moj CV",
   contactSection: "Kako stupiti u kontakt sa mnom",
   projects: "Projekte koje bih izdvojio",
+};
+
+export const eng = {
+  navigationItem: ["About me", "CV", "Tools", "Contact", "Projects"],
+  headerTitle: ["Hello, I am", "Boris Blagojevic"],
+  toolsSection: "Languages and tools I use",
+  aboutSection: [
+    "About me",
+    "Born in  Banja Luka.",
+    "I graduate electro technical high school in Banja Luka as telecommunication technician.",
+    "I'm currently studying software engineering at Pan-Evropian University Apeiron in Banja Luka.",
+  ],
+  cvSection: "My resume",
+  contactSection: "Let's get in touch",
+  projects: "Some of my projects",
 };
