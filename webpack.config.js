7a12b37b15df9c84a310623fa12f12c91797d098
blagojevic <boris.blagojevicc@hotmail.com>
2025-07-19 11:31:44 +0200

    #19 WEBPACK ASSET LOADING

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,57 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/js/main.js',
     devServer: {
         static: path.resolve(__dirname, './dist'),
         port: 3333,
+        hot: true,
+        client: {
+            overlay: {
+                errors: true,
+                warnings: false,
+                runtimeErrors: true
+            }
+        }
     },
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist')
     },
     module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    process.env.NODE_ENV !== 'production'
+                        ? 'style-loader'
+                        : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|webm|pdf)$/i,
+                type: 'asset/resource'
+            }
+        ]
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.js']
     },
-    plugins: [],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'index.html'),
+            favicon: path.resolve(__dirname, './src/images/favicon/bbFavicon.svg'),
+            filename: '../dist/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        })
+    ],
 }
