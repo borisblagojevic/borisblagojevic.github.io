9147a39e67acaf298e725d733c658e15eb39e326
borisblagojevic <boris.blagojevic@hotmail.com>
2025-11-04 14:57:00 +0100

    up

--- webpack.config.js
+++ webpack.config.js
@@ -7,15 +7,19 @@ module.exports = {
     entry: './src/js/main.js',
     devServer: {
         static: path.resolve(__dirname, './dist'),
-        port: 333,
+        port: 3333,
     },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, './dist'),
+        // Add this line to correctly handle asset paths
         assetModuleFilename: 'assets/[name][ext]',
     },
     module: {
         rules: [
+
+
+            // 1. SCSS/CSS Rule (Kept your fixed version)
             {
                 test: /\.s[ac]ss$/i,
                 use: [
@@ -24,17 +28,21 @@ module.exports = {
                     'sass-loader'
                 ]
             },
+            // 2. Images Rule (Uses Asset Module)
             {
                 test: /\.(png|jpe?g|gif|svg)$/i,
-                type: 'asset/resource', 
+                type: 'asset/resource', // emits a separate file and exports the URL
                 generator: {
-                    filename: 'public/images/[name][ext]'
+                    // Optional: customize the output directory for images
+                    filename: 'images/[name][ext]'
                 }
             },
+            // 3. Video/WebM Rule (Uses Asset Module)
             {
                 test: /\.(webm|mp4|mov)$/i,
-                type: 'asset/resource',
+                type: 'asset/resource', // emits a separate file and exports the URL
                 generator: {
+                    // Optional: customize the output directory for videos
                     filename: 'videos/[name][ext]'
                 }
             }
@@ -51,6 +59,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             favicon: path.resolve(__dirname, './src/images/favicon/bbFavicon.svg'),
             template: path.resolve(__dirname, './index.html'),
+            // Simplified filename path (as recommended in the prior response)
             filename: 'index.html',
         })
     ],
